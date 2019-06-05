c97081f19465816df059ea74af128c3c02582767
Dom <DomKiki42@gmail.com>
2019-06-05 14:25:09 +0200

    Modular
    Z-axis added.
    
    ruleSet is modifyable (although there seems to be a problem, try B4/S9 compared to yt video v=EW9Q0qMc2Xc, doesn't match)
    
    Next up should really be colors according to neighbours/lifespan, this is really not aesthetic to have all tiles in black.

--- p5Game.js
+++ p5Game.js
@@ -1,23 +1,32 @@
 var p5Game = function(p) {
 
-    var gridSize = { h: 50, w: 50, d: 0 },
+    const CAMERA = 0,
+          BRUSH  = 1;
+    var   state  = CAMERA;
+
+    var gridSize = { h: 10, w: 10, d: 3 },
         grid,
-        tileSize = 10,
+        tileSize = 20,
         neighbourhoodRadius = 1;
 
-    var thresholds = { death: 2, neutral: 2, life: 3 };
+    var rules = { birth: [4/*3*/], survive: [9/*2,3*/] };
 
-    var easycam,
+    var easycam, camState,
         Controls, ctrl, gui,
-        btnPause, paused = false;
+        btnPause, paused = false,
+        btnBrush;
 
     var backgroundColor;
+
+    /********************************************************** p5 *********************************************************/
 
     p.setup = function() {
 
         var canvas = p.createCanvas(800, 800, p.WEBGL);
         backgroundColor = p.color("#e6ffff");
         
+        grid = p.initGrid(gridSize, 0.3);
+
         // fix for EasyCam to work with p5 v0.7.2
         Dw.EasyCam.prototype.apply = function(n) {
             var o = this.cam;
@@ -26,10 +35,12 @@ var p5Game = function(p) {
 	    };
         p.pixelDensity(1);
         p.setAttributes('antialias', true);
-        easycam = p.createEasyCam({distance: 800});
 
-        grid = p.initGrid(gridSize, 0.3);
+        easycam = p.createEasyCam({distance: 800});
         p.initGUI();
+        p.enableCamera(true);
+
+        p.reset();
 
     }
 
@@ -40,115 +51,212 @@ var p5Game = function(p) {
         p.frameRate(ctrl.fps);
         p.rotateX(10);
         p.background(backgroundColor);
-        p.strokeWeight(1);
+        p.strokeWeight(0.5);
         
         grid = p.iterate(grid);
         p.drawGrid();
 
     }
 
+    /********************************************************* Game ********************************************************/
+
     p.iterate = function(gr) {
 
         let g = p.initGrid(gridSize, 0);
         for (let i = 0; i < g.length; i++)
-            for (let j = 0; j < g[i].length; j++) {
-                let n = p.neighbourhood(gr,i,j);
-                if ((n < thresholds.death) || (n > thresholds.life))
-                    g[i][j] = 0;
-                else if (n == thresholds.life)
-                    g[i][j] = 1;
-                else if (n == thresholds.neutral)
-                    g[i][j] = gr[i][j];
+            for (let j = 0; j < g[i].length; j++) 
+                for (let k = 0; k < g[i][j].length; k++) {
+                    let n = p.neighbourhood(gr,i,j,k);
+                    if (rules.birth.includes(n))
+                        g[i][j][k] = 1;
+                    else if (rules.survive.includes(n))
+                        g[i][j][k] = gr[i][j][k];
+                    else
+                        g[i][j][k] = 0;
             }
         return g;
     }
 
-    p.neighbourhood = function(g,x,y) {
+    p.neighbourhood = function(g,x,y,z) {
 
-        let n = 0;
+        let n = 0,
+            checked = p.initGrid(gridSize, 0);
         for (let i = (x - neighbourhoodRadius); i <= (x + neighbourhoodRadius); i++)
             for (let j = (y - neighbourhoodRadius); j <= (y + neighbourhoodRadius); j++)
-                if ((i != x) || (j != y)) {
-                    let r = p.modulus(i, gridSize.h);
-                    let c = p.modulus(j, gridSize.w);
-                    if (g[r][c] == 1)
-                        n++;
+                for (let k = (z - neighbourhoodRadius); k <= (z + neighbourhoodRadius); k++) {
+                    let row = p.modulus(i, gridSize.h);
+                    let col = p.modulus(j, gridSize.w);
+                    let dep = p.modulus(k, gridSize.d);
+                    if ((g[row][col][dep] == 1) && (checked[row][col][dep] != 1))
+                        if ((row != x) || (col != y) || (dep != z))
+                            n++;
+                    checked[row][col][dep] = 1;
                 }
         return n;
 
     }
 
+    p.updateRuleSet = function(rs) {
+
+        let bs  = rs.split("/"),
+            res = { birth: [], survive: [] };
+
+        if (bs.length != 2)
+            return;
+
+        for (let i = 0; i < bs.length; i++) {
+
+            let t   = bs[i].charAt(0),
+                arr = [];
+            for (let j = 1; j < bs[i].length; j++) {
+                let c = parseInt(bs[i].charAt(j));
+                if ((!arr.includes(c)) && (!isNaN(c)))
+                    arr.push(c);
+            }
+
+            if      (t == 'B') res.birth   = arr;
+            else if (t == 'S') res.survive = arr;
+        }
+
+        if ((res.birth.length > 0) && (res.survive.length > 0))
+            rules = res;
+
+    }
+
+    p.ruleSetString = function() {
+        let b = "",
+            s = "";
+        for (let v of rules.birth)   b += String(v);
+        for (let v of rules.survive) s += String(v);
+        return "B" + b + "/S" + s;
+    }
+
+    /********************************************************* Grid ********************************************************/
+
     p.initGrid = function(gridSize, r) {
         let g = [];
-        for (let i = 0; i < gridSize.h; i++)
-            g.push(new Array(gridSize.w).fill(0));
+        for (let i = 0; i < gridSize.h; i++) {
+            let arrY = [];
+            for (let j = 0; j < gridSize.w; j++) {
+                let arrZ = [];
+                for (let k = 0; k < gridSize.d; k++)
+                    arrZ.push(0);
+                arrY.push(arrZ);
+            }
+            g.push(arrY);
+        }
+
         return p.randomizeGrid(g, r);
     }
-
-    p.resizeGrid = function(gr, ctrl) {
-
-        // Grid
-        let g = gr.slice();
-        // X
-        while (g.length != ctrl.h)
-            if (g.length < ctrl.h)
-                g.push(new Array(ctrl.w).fill(0));
-            else
-                g.pop();
-        gridSize.h = ctrl.h;
-        // Y
-        for (let i = 0; i < g.length; i++)
-        while (g[i].length != ctrl.w)
-            if (g[i].length < ctrl.w)
-                g[i].push(0);
-            else
-                g[i].pop();
-        gridSize.w = ctrl.w;
-
-        // Camera
-        easycam.setDistance(p.max([ctrl.w, ctrl.h, ctrl.d]) * 15);
-
-        return g;
-    }
-
+    
     p.randomizeGrid = function(g,r) {
         let gr = g.slice();
         for (let i = 0; i < g.length; i++)
             for (let j = 0; j < g[i].length; j++)
-                if (p.random(1) < r)
-                    g[i][j] = 1;
+                for (let k = 0; k < g[i][j].length; k++)
+                    if (p.random() < r)
+                        g[i][j][k] = 1;
         return gr;
     }
 
-    p.drawGrid = function() {
-        p.translate(-0.5 * gridSize.w * tileSize, -0.5 * gridSize.h * tileSize);
-        for (let i = 0; i < grid.length; i++) {
-            for (let j = 0; j < grid[i].length; j++) {
-                let color  = (grid[i][j] == 1) ? p.color(0) : p.color(255);
-                p.fill(color);
-                p.translate(tileSize, 0, 0);
-                p.box(tileSize);
+    p.resizeGrid = function(gr, ctrl) {
+        
+        // Grid
+        let g = gr.slice();
+        // X
+        while (g.length != ctrl.h)
+            if (g.length < ctrl.h) g.push([]);
+            else g.pop();
+        // Y
+        for (let i = 0; i < g.length; i++) {
+            while (g[i].length != ctrl.w)
+                if (g[i].length < ctrl.w) g[i].push([]);
+                else g[i].pop();
+            // Z
+            for (let j = 0; j < g[i].length; j++)
+                while (g[i][j].length != ctrl.d)
+                    if (g[i][j].length < ctrl.d) g[i][j].push(0);
+                    else g[i][j].pop();
             }
-            p.translate(-tileSize * gridSize.w, tileSize, 0);
+              
+        // Dimensions
+        gridSize.d = ctrl.d;
+        gridSize.h = ctrl.h;
+        gridSize.w = ctrl.w;
+                
+        // Camera
+        easycam.setDistance(p.max([ctrl.w, ctrl.h, ctrl.d]) * tileSize * 2);
+
+        return g;
+    }
+
+    p.drawGrid = function() {
+        p.stroke(p.color(0));
+        p.translate(-0.5 * grid[0].length * tileSize, -0.5 * grid.length * tileSize, -0.5 * grid[0][0].length * tileSize);
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i].length; j++)  {
+                for (let k = 0; k < grid[i][j].length; k++) {
+                    if (grid[i][j][k]) p.fill(p.color(0));
+                    else p.noFill();
+                    p.translate(0, 0, tileSize);
+                    p.box(tileSize);
+                }
+                p.translate(tileSize, 0, -tileSize * grid[i][j].length);
+            }
+            p.translate(-tileSize * grid[i].length, tileSize, 0);
         }
     }
 
-    p.modulus = function(a,b) { return (a + b) % b; }
+    /******************************************************** Camera*********************************************************/
+
+    p.enableCamera = function(b) {
+        let scl, lbl;
+        if (b) {
+            scl = 0.0005;
+            lbl = "Brush";
+        }
+        else {
+            scl = 0;
+            lbl = "Camera";
+        }
+        easycam.setRotationScale(scl);
+        easycam.setZoomScale(scl);
+        //btnBrush.name(lbl);
+    }
+
+    /******************************************************** States *******************************************************/
+
+    p.brushCamera = function() {
+        state = (state + 1) % 2;
+        switch (state) {
+            case CAMERA:
+                p.enableCamera(true);
+                break;
+            case BRUSH:
+                p.enableCamera(false);
+                break;
+            default:
+                return;
+        }
+    }
 
     p.pauseResume = function() {
         paused = !paused;
-        if (paused) 
-            p.noLoop();
-        else
-            p.loop();
+        if (paused) p.noLoop();
+        else        p.loop();
         btnPause.name((paused) ? "Resume" : "Pause");
     }
 
-    p.reset = function() {
-        grid = p.initGrid(gridSize, ctrl.population);
-        p.translate(0.5 * gridSize.w * tileSize, -0.5 * gridSize.h * tileSize);
+    p.reset = function() { grid = p.initGrid(gridSize, ctrl.population); 
+        // Debugging purposes, delete afterwards
+        grid[5][5][1] = 1;
+        grid[5][5][2] = 1;
+        grid[5][6][1] = 1;
+        grid[5][6][2] = 1; 
         p.drawGrid();
     }
+
+    /********************************************************* GUI *********************************************************/
 
     p.initGUI = function() {
 
@@ -157,13 +265,14 @@ var p5Game = function(p) {
             this.w           = gridSize.w;
             this.h           = gridSize.h;
             this.d           = gridSize.d;
-            this.test ="--------------------";
             this.nghbrRadius = 1;
+            this.ruleSet     = p.ruleSetString();
             this.deathThresh = 2;
             this.neutral     = 2;
             this.lifeThresh  = 3;
+            this.brushCamera = function() { p.brushCamera(); }
             this.population  = 0.3;
-            this.fps         = 30;
+            this.fps         = 1/*30*/;
             this.pauseResume = function() { p.pauseResume(); }
             this.reset       = function() { p.reset(); };
         };
@@ -171,19 +280,19 @@ var p5Game = function(p) {
 
         gui  = new dat.GUI();
         // Grid dimensions Sliders
-        gui.add(ctrl, "w", 0, 100, 1)
+        gui.add(ctrl, "w", 1, 50, 1)
            .name("Grid width")
            .onChange(function(value) {
                 ctrl.w = value;
                 grid   = p.resizeGrid(grid, ctrl);
             });
-        gui.add(ctrl, "h", 0, 100, 1)
+        gui.add(ctrl, "h", 1, 50, 1)
            .name("Grid height")
            .onChange(function(value) {
                 ctrl.h = value;
                 grid   = p.resizeGrid(grid, ctrl);
             });
-        gui.add(ctrl, "d", 0, 100, 1)
+        gui.add(ctrl, "d", 1, 50, 1)
            .name("Grid depth")
            .onChange(function(value) {
                 ctrl.d = value;
@@ -194,22 +303,21 @@ var p5Game = function(p) {
         gui.add(ctrl, "nghbrRadius", 1, 5, 1)
            .name("Neighbourhood")
            .onChange(function(value) { neighbourhoodRadius = value; });
-        gui.add(ctrl, "deathThresh", 0, 20, 1)
-           .name("Death Threshold")
-           .onChange(function(value) { thresholds.death = value; });
-        gui.add(ctrl, "neutral", 0, 20, 1)
-           .name("Neutral")
-           .onChange(function(value) { thresholds.neutral = value; });
-        gui.add(ctrl, "lifeThresh", 0, 20, 1)
-           .name("Life Threshold")
-           .onChange(function(value) { thresholds.life = value; });
+        gui.add(ctrl, "ruleSet")
+           .name("Rules Set")
+           .onChange(function(value) { p.updateRuleSet(value) });
 
         // Simulation control
+        //btnBrush = gui.add(ctrl, "brushCamera").name("Brush");
         gui.add(ctrl, "fps", 1, 60).name("Max FpS");
         btnPause = gui.add(ctrl, "pauseResume").name("Pause");
         gui.add(ctrl, "population", 0, 1).name("Population Ratio");
-        gui.add(ctrl, "reset").name("Reset");
+        gui.add(ctrl, "reset").name("Populate");
 
     }
+
+    /******************************************************** Misc *********************************************************/
+
+    p.modulus = function(a,b) { return (a + b) % b; }
 
 }
